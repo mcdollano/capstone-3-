19fae826688901309179470aedc6c66c5c991589
mcdollano <mcdollano@gmail.com>
2017-08-04 16:36:40 +0800

    analytics

--- resources/views/view_article.blade.php
+++ resources/views/view_article.blade.php
@@ -1,6 +1,20 @@
 @extends("home_template")
 	@section("view_article")
 
+<!--GOOGLE ANALYTICS -->
+<!-- Step 1: Load the Embed API library. -->
+
+	<script>
+(function(w,d,s,g,js,fs){
+  g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+  js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+  js.src='https://apis.google.com/js/platform.js';
+  fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+}(window,document,'script'));
+</script>
+
+
+
 		<!--FACEBOOK SHARE-->
 
 			<script>
