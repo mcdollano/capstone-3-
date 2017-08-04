81d19836b4fdf183a27b917fba154047055f4bef
mcdollano <mcdollano@gmail.com>
2017-08-04 15:37:40 +0800

    share button

--- resources/views/home_template.blade.php
+++ resources/views/home_template.blade.php
@@ -15,7 +15,7 @@
    <meta property="og:type"          content="website" />
   <meta property="og:title"         content="{{$blogs->title}}" />
   <meta property="og:description"   content="{{$blogs->caption}}" />
-  <meta property="og:image"         content='{{URL:: to($blogs->banner_image) }}' />
+  <meta property="og:image:url"         content='{{URL:: to($blogs->banner_image) }}' />
 </head>
 
     <!-- Styles -->
